2ebc8ceefac51d583ff8d9eae512c37df98e52fd
hl1255 <hlmilky125@gmail.com>
2025-03-07 01:13:56 +0800

    Add files via upload

---
+++ public/assets/include/footer.php
@@ -0,0 +1,7 @@
+<footer class="text-center py-3 bg-dark text-light">
+    <p>&copy; 2025 BlogWebsite. All Rights Reserved.</p>
+</footer>
+
+<!-- Dark Mode Script -->
+<script src="/public/assets/js/script.js"></script>
+
